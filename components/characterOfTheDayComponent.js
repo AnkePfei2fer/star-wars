9ff2dbe3615f8b7abaef72d10e08c629aaa6f29a
Anke <pfeiffer.anke@outlook.com>
2021-10-22 16:18:27 +0200

    :sparkles: Add random character of the day

---
+++ components/characterOfTheDayComponent.js
@@ -0,0 +1,28 @@
+import createElement from "../lib/elements";
+import styles from "./characterOfTheDayComponent.module.css";
+
+// Declare the function for creating card components
+export function createCharacterOfTheDayComponent(randomNumber) {
+  const picture = createElement("img", {
+    classname: styles.picture,
+    src: `https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${randomNumber}.jpg`,
+  });
+
+  //   const cardTitle = createElement("h2", {}, name);
+  //   const homeworldElement = createElement("span", {}, homeworld);
+  //   const characterElement = createElement(
+  //     "article",
+  //     { className: styles.card },
+  //     [picture, cardTitle, homeworldElement]
+  //   );
+
+  //   // Fetch Homeworld information from API
+  //   fetch(homeworld)
+  //     .then((response) => response.json())
+  //     .then((body) => {
+  //       homeworldElement.textContent = body.name;
+  //     });
+
+  //   return characterElement;
+  return picture;
+}
--- main.js
+++ main.js
@@ -2,14 +2,20 @@ import "./style.css";
 import { createMainElement } from "./lib/mainElement";
 import { createFooterElement } from "./lib/footerElement";
 import { createHeaderElement } from "./lib/headerElement";
-import fetchCharacters from "./lib/fetchAPI";
+import { fetchCharacters } from "./lib/fetchAPI";
+import { fetchRandomCharacter } from "./lib/fetchAPI";
 
 const appElement = document.body.querySelector("#app");
+
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomInt(84);
 
 // Fetch character data from SWAPI
 const characters = await fetchCharacters();
 const headerElement = createHeaderElement();
-const mainElement = createMainElement(characters);
+const mainElement = createMainElement(characters, randomNumber);
 const footerElement = createFooterElement();
 
 appElement.append(headerElement, mainElement, footerElement);
