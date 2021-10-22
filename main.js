1f69ee3674a399411ee26ad86dbd8cfa280b9b96
Anke <pfeiffer.anke@outlook.com>
2021-10-22 11:04:34 +0200

    :sparkles: add header and background image

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import { createMainElement } from "./lib/mainElement";
 import { createFooterElement } from "./lib/footerElement";
+import { createHeaderElement } from "./lib/headerElement";
 
 const appElement = document.body.querySelector("#app");
 
+const headerElement = createHeaderElement();
 const mainElement = createMainElement();
 const footerElement = createFooterElement();
 
-appElement.append(mainElement, footerElement);
+appElement.append(headerElement, mainElement, footerElement);
