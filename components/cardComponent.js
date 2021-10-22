a4ac90f8ead5f39eca4996b2242e63959f483dee
Anke <pfeiffer.anke@outlook.com>
2021-10-22 11:49:44 +0200

    :sparkles: Fetch characters from Star Wars Api

--- components/cardComponent.js
+++ components/cardComponent.js
@@ -1,10 +1,10 @@
 import createElement from "../lib/elements";
 import styles from "./cardComponent.module.css";
 
-export function createCardComponent() {
-  const cardTitle = createElement("h2", {}, ["Luke Skywalker"]);
-  const cardComponent = createElement("article", { className: styles.card }, [
+export function createCardComponent({ name }) {
+  const cardTitle = createElement("h2", {}, name);
+  const cardElement = createElement("article", { className: styles.card }, [
     cardTitle,
   ]);
-  return cardComponent;
+  return cardElement;
 }
--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import "./style.css";
 import { createMainElement } from "./lib/mainElement";
 import { createFooterElement } from "./lib/footerElement";
 import { createHeaderElement } from "./lib/headerElement";
+import fetchCharacters from "./lib/fetchAPI";
 
 const appElement = document.body.querySelector("#app");
 
+// Fetch character data from SWAPI
+const characters = await fetchCharacters();
+
 const headerElement = createHeaderElement();
-const mainElement = createMainElement();
+const mainElement = createMainElement(characters);
 const footerElement = createFooterElement();
 
 appElement.append(headerElement, mainElement, footerElement);
