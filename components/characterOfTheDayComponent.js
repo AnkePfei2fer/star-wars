c0cf87cda70e827202eadedb93e23d75c8d1d079
Anke <pfeiffer.anke@outlook.com>
2021-10-25 12:12:10 +0200

    :sparkles: Add name of COTD on hovering, add responsive design

--- components/characterOfTheDayComponent.js
+++ components/characterOfTheDayComponent.js
@@ -2,27 +2,25 @@ import createElement from "../lib/elements";
 import styles from "./characterOfTheDayComponent.module.css";
 
 // Declare the function for creating card components
-export function createCharacterOfTheDayComponent(randomNumber) {
-  const picture = createElement("img", {
-    className: styles.picture,
-    src: `https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${randomNumber}.jpg`,
-  });
+export function createCharacterOfTheDayComponent(
+  randomNumber,
+  randomCharacter
+) {
+  const characterOfTheDay = createElement(
+    "div",
+    { className: styles.characterOfTheDay },
+    [
+      createElement("img", {
+        className: styles.picture,
+        src: `https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${randomNumber}.jpg`,
+      }),
 
-  //   const cardTitle = createElement("h2", {}, name);
-  //   const homeworldElement = createElement("span", {}, homeworld);
-  //   const characterElement = createElement(
-  //     "article",
-  //     { className: styles.card },
-  //     [picture, cardTitle, homeworldElement]
-  //   );
-
-  //   // Fetch Homeworld information from API
-  //   fetch(homeworld)
-  //     .then((response) => response.json())
-  //     .then((body) => {
-  //       homeworldElement.textContent = body.name;
-  //     });
-
-  //   return characterElement;
-  return picture;
+      createElement(
+        "h2",
+        { className: styles.name, id: styles.name },
+        randomCharacter
+      ),
+    ]
+  );
+  return characterOfTheDay;
 }
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import "./style.css";
+import { createHeaderElement } from "./lib/headerElement";
+import { createTopElement } from "./lib/mainElement";
 import { createMainElement } from "./lib/mainElement";
 import { createFooterElement } from "./lib/footerElement";
-import { createHeaderElement } from "./lib/headerElement";
 import { fetchCharacters } from "./lib/fetchAPI";
-// import { fetchRandomCharacter } from "./lib/fetchAPI";
+import { fetchRandomCharacter } from "./lib/fetchAPI";
 
 const appElement = document.body.querySelector("#app");
 
@@ -11,11 +12,14 @@ function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
 }
 const randomNumber = getRandomInt(84);
+console.log(randomNumber);
 
 // Fetch character data from SWAPI
 const characters = await fetchCharacters();
+const randomCharacter = await fetchRandomCharacter(randomNumber);
 const headerElement = createHeaderElement();
-const mainElement = createMainElement(characters, randomNumber);
+const topElement = createTopElement(randomNumber, randomCharacter);
+const mainElement = createMainElement(characters);
 const footerElement = createFooterElement();
 
-appElement.append(headerElement, mainElement, footerElement);
+appElement.append(headerElement, topElement, mainElement, footerElement);
